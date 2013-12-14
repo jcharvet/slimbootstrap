c3837608665b516d0b8a9579d7537964b8006343
jcharvet <charvet.jerome@gmail.com>
2013-12-14 02:04:07 +0000

    setup database and mysql driver

---
+++ config/config.php
@@ -0,0 +1,23 @@
+<?php
+$basedir = __DIR__ . '/../';
+
+return array(
+// Environment
+    'php.error_reporting' => E_ALL,
+    'php.display_errors'  => true,
+    'php.log_errors'      => true,
+    'php.error_log'       => $basedir . 'logs/errors.txt',
+    'php.date.timezone'   => 'Europe/London',
+
+// SQLite
+//    'db.dsn'              => 'sqlite:' . $basedir . 'db/sqlite.db',
+
+// MySQL
+    'db.dsn'              => 'mysql:host=localhost;dbname=test',
+    'db.username'         => 'root',
+    'db.password'         => 'root',
+
+// Application paths
+    'path.routes'         => $basedir . 'routes/',
+    'path.templates'      => $basedir . 'templates/'
+);
